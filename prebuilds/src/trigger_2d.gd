85344ab090bc01b76b6b6e60f70c0b6731383d42
Rudolf Vrbensky <89221002+XFajk@users.noreply.github.com>
2025-07-22 10:35:02 +0200

    Initial commit

---
+++ prebuilds/src/trigger_2d.gd
@@ -0,0 +1,41 @@
+extends Area2D
+class_name Trigger2D
+
+@export var trigger_node: PhysicsBody3D = null
+@export var trigger_once: bool = false
+@export var retrigger_duration_seconds: float = 1.0
+
+@onready var timer: Timer = Timer.new()
+
+func _ready() -> void:
+	if retrigger_duration_seconds:
+		timer.wait_time = retrigger_duration_seconds
+		timer.one_shot = true
+		timer.timeout.connect(_on_timeout)
+		
+		add_child(timer, false, Node.INTERNAL_MODE_BACK)
+	
+	body_entered.connect(_on_body_entered)
+	
+func _on_body_entered(body: Node2D) -> void:
+	if body == trigger_node or trigger_node == null:
+		for child in get_children():
+			if child is CollisionShape2D:
+				continue
+			
+			if not child.has_method("_on_trigger"):
+				push_warning("Child %s of trigger %s is missing _on_trigger(). Please add this method." % [child.name, self.name])
+				continue
+								
+			child._on_trigger(body)
+			
+		if trigger_once:
+			queue_free()
+		else: 
+			if retrigger_duration_seconds:
+				body_entered.disconnect(_on_body_entered)
+				timer.start()
+			
+func _on_timeout() -> void:
+	body_entered.connect(_on_body_entered)
+	
